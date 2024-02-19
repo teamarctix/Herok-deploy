4e566097522bd5d19ea9e9003f219c05a22aae83
arctixinc <inc.arctix@gmail.com>
2024-02-20 00:10:16 +0530

    push

--- Heroku/modules/start.py
+++ Heroku/modules/start.py
@@ -9,11 +9,17 @@ from config import OWNER_ID, HEROKU_API
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pymongo import MongoClient
 
-
 START_MESSAGE = "HEY, I AM A BOT"
 START_BUTTONS = [
     [
-         InlineKeyboardButton('MORE', callback_data="Test")
+        InlineKeyboardButton('MORE', callback_data="Test")
+    ]
+]
+
+END_BUTTON = [
+    [
+        InlineKeyboardButton('MENU', callback_data="Go to Menu"),
+        InlineKeyboardButton('MENU', callback_data="Go to Menu")
     ]
 ]
 
@@ -24,19 +30,12 @@ START_BUTTONS = [
 async def start_command(client, message):
     try:
         await message.reply_text(START_MESSAGE, reply_markup=InlineKeyboardMarkup(START_BUTTONS))
-        
     except Exception as e:
         print(f"Error: {e}")
-        
+
 @app.on_callback_query()
 async def callback_query(client, CallbackQuery):
     if CallbackQuery.data == "MORE":
-        END_BUTTON = [
-            [
-                InlineKeyboardButton('MENU', callback_data="Go to Menu"),
-                InlineKeyboardButton('MENU', callback_data="Go to Menu")
-            ]
-        ]
         CallbackQuery.edit_message_text(
             START_MESSAGE,
             reply_markup=InlineKeyboardMarkup(END_BUTTON)
